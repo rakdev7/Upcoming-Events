97f9151c28293198bb2fa88a1c6d6035c31076c9
rakdev7 <rakdev7@gmail.com>
2019-08-06 12:15:23 -0500

    Adding comments about the current state of the solution.

--- Upcoming Events/ConflictChecker.swift
+++ Upcoming Events/ConflictChecker.swift
@@ -11,4 +11,16 @@ import Foundation
 //Ideal place for the conflict checker algorithm.
 func checkForConflicts() {
     
+    /*
+     The gist of the problem is to find meetings that are conflicting to each other and somehow indicate the conflicting events on the UI.
+     
+     My current solution has limitations and is not perfect and doesn't cover some edge cases.
+     
+     Need help in writing a new algorithm or refining my implementation to solve this problem. (I did find multiple implementations of this problem in Java but nothing in Swift so far.)
+     
+     Some Java references - https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=2ahUKEwjknPnc3O7jAhVyT98KHWzgCoQQFjABegQIBRAB&url=http%3A%2F%2Fkarmaandcoding.blogspot.com%2F2012%2F01%2Ffind-all-conflicting-appointments-from.html&usg=AOvVaw2d0-Gl5IfKGQYuGvwK9319
+    
+     */
+    
+    
 }
